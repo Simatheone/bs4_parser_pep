e35ccfbcf217fb4ded6ea171fbcaa935b804d650
simatheone <samone07@mail.ru>
2022-07-17 23:48:47 +0300

    Add url to peps page

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEP_CATALOG = 'https://peps.python.org/'
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
 LOG_DT_FORMAT = '%d.%m.%Y %H:%M:%S'
