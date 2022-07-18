8c9a64acee82b7390bd3c47877b510d96c78c61a
simatheone <samone07@mail.ru>
2022-07-18 20:27:51 +0300

    Add function which compares peps' statuses

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ from requests import RequestException
 
 from exceptions import ParserFindTagException
 
+
 def get_response(session, url):
     try:
         response = session.get(url)
@@ -14,6 +15,7 @@ def get_response(session, url):
             stack_info=True
         )
 
+
 def find_tag(soup, tag, attrs=None):
     searched_tag = soup.find(tag, attrs=(attrs or {}))
     if searched_tag is None:
@@ -21,3 +23,16 @@ def find_tag(soup, tag, attrs=None):
         logging.error(error_msg, stack_info=True)
         raise ParserFindTagException(error_msg)
     return searched_tag
+
+
+def compare_peps_statuses(
+    pep_0_page_status, peps_card_status, link_to_pep_card
+):
+    if peps_card_status not in pep_0_page_status:
+        log_message = f"""
+            Несовпадающие статусы:
+            {link_to_pep_card}
+            Статус в карточке: {peps_card_status}
+            Ожидаемые статусы: {pep_0_page_status}
+        """
+        logging.info(log_message)
