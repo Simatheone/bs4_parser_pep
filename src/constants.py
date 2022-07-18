f170e5f387d70b4b0256a4e8659d07f89e69b740
simatheone <samone07@mail.ru>
2022-07-18 20:26:44 +0300

    Add headers for tables: whats-new, latest-version, pep

--- src/constants.py
+++ src/constants.py
@@ -7,12 +7,15 @@ DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
 LOG_DT_FORMAT = '%d.%m.%Y %H:%M:%S'
 EXPECTED_STATUS = {
-    'A': ['Active', 'Accepted'],
-    'D': ['Deferred'],
-    'F': ['Final'],
-    'P': ['Provisional'],
-    'R': ['Rejected'],
-    'S': ['Superseded'],
-    'W': ['Withdrawn'],
-    '': ['Draft', 'Active'],
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
 }
+WHATS_NEW_TABLE_HEADER = ('Ссылка на статью', 'Заголовок', 'Редактор, Автор')
+LATEST_VERSION_TABLE_HEADER = ('Ссылка на документацию', 'Версия', 'Статус')
+PEP_TABLE_HEADER = ('Статус', 'Количество')
