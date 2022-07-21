d33a49652c96fe51084685b928083b31eb3e1067
simatheone <samone07@mail.ru>
2022-07-21 19:48:13 +0300

    Add an exception UnexpectedPEPStatus

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,8 @@
 class ParserFindTagException(Exception):
     """Exception calls when parser could not find HTML tag."""
     pass
+
+
+class UnexpectedPEPStatus(Exception):
+    """Exception calls when unexpected status has been found."""
+    pass
