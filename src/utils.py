d2e0f1a418d3e170b28671713062bb3d488548a7
simatheone <samone07@mail.ru>
2022-07-21 19:47:48 +0300

    Add docstrings for functions get_response, find_tag

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,11 @@ from exceptions import ParserFindTagException
 
 
 def get_response(session, url):
+    """Function makes request to url.
+    If response has been got returns it.
+    Otherwise raises RequestException and writes
+    information in logs, log level exception.
+    """
     try:
         response = session.get(url)
         response.encoding = 'utf-8'
@@ -17,6 +22,12 @@ def get_response(session, url):
 
 
 def find_tag(soup, tag, attrs=None):
+    """Function looks for a tag.
+    If tag has been found returns it.
+    Otherwise raises ParserFindTagException and writes
+    information in logs, log level error.
+
+    """
     searched_tag = soup.find(tag, attrs=(attrs or {}))
     if searched_tag is None:
         error_msg = f'Не найден тег {tag} {attrs}'
