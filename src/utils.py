a66d0abbd5a796cf7b6cea4977e77c984ca382ea
simatheone <samone07@mail.ru>
2022-07-21 19:20:05 +0300

    Move compare func to main.py

--- src/utils.py
+++ src/utils.py
@@ -23,16 +23,3 @@ def find_tag(soup, tag, attrs=None):
         logging.error(error_msg, stack_info=True)
         raise ParserFindTagException(error_msg)
     return searched_tag
-
-
-def compare_peps_statuses(
-    pep_0_page_status, peps_card_status, link_to_pep_card
-):
-    if peps_card_status not in pep_0_page_status:
-        log_message = f"""
-            Несовпадающие статусы:
-            {link_to_pep_card}
-            Статус в карточке: {peps_card_status}
-            Ожидаемые статусы: {pep_0_page_status}
-        """
-        logging.info(log_message)
